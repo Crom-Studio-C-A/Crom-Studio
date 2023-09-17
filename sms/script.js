9535890a0835c566bd691ee40ef1a6c8a3b8af17
Angel Romero <mrprogramador21@gmail.com>
2023-09-17 15:26:52 -0400

    script

--- sms/script.js
+++ sms/script.js
@@ -9,13 +9,16 @@
 
         // Supongamos que la autenticación fue exitosa
         const authenticationSuccessful = true;
-    
-        if (authenticationSuccessful) {
-            // Redirige al usuario al panel de control (dashboard.html)
-            window.location.href = 'dashboard.html';
-        } else {
-            // Maneja el caso en que la autenticación falla
-            alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
-        }
-    });
-    
+
+    if (authenticationSuccessful) {
+        // Almacena las credenciales en el almacenamiento local
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+
+        // Redirige al usuario al panel de control (dashboard.html)
+        window.location.href = 'dashboard.html';
+    } else {
+        // Maneja el caso en que la autenticación falla
+        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+    }
+});
