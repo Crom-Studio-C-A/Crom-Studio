2c2c4f384721865b1ae4d93aab816617c7fb7a9e
Angel Romero <mrprogramador21@gmail.com>
2023-09-17 15:24:14 -0400

    script.js

--- sms/script.js
+++ sms/script.js
@@ -1,23 +1,21 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+    
+        // Realiza la autenticación con tu API
+        const authURL = `http://www.interconectados.net/api2/get.asp?user=${username}&password=${password}&get=status`;
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Construir la URL para la autenticación
-    const authURL = `http://www.interconectados.net/api2/get.asp?user=${username}&password=${password}&get=status`;
-
-    // Realizar la solicitud HTTP GET
-    fetch(authURL)
-        .then(response => response.json())
-        .then(data => {
-            // Aquí puedes manejar la respuesta de la API
-            console.log(data);
-
-            // Redirigir a la siguiente página o realizar otras acciones según la respuesta
-        })
-        .catch(error => {
-            // Manejar errores de la solicitud
-            console.error('Error:', error);
-        });
-});
+        // Supongamos que la autenticación fue exitosa
+        const authenticationSuccessful = true;
+    
+        if (authenticationSuccessful) {
+            // Redirige al usuario al panel de control (dashboard.html)
+            window.location.href = 'dashboard.html';
+        } else {
+            // Maneja el caso en que la autenticación falla
+            alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+        }
+    });
+    
