655ddcd08d0b1de5506aafeb81edac08725c7820
Angel Romero <mrprogramador21@gmail.com>
2023-09-17 17:37:10 -0400

    s

--- sms/script.js
+++ sms/script.js
@@ -3,43 +3,27 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // URL para autenticar al usuario
-    const authURL = `https://www.interconectados.net/api2/get.asp?user=${username}&password=${password}&get=status`;
+    // URL para autenticar al usuario con JSONP
+    const authURL = `https://www.interconectados.net/api2/get.asp?user=${username}&password=${password}&get=status&callback=handleAuth`;
 
-    fetch(authURL)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                // Autenticación exitosa, mostrar el panel de envío de SMS y saldo de créditos
-                document.querySelector('.login-container').classList.add('hidden');
-                document.querySelector('.sms-container').classList.remove('hidden');
-                document.getElementById('credits').classList.remove('hidden');
+    // Agregar una función de callback para manejar la respuesta
+    window.handleAuth = function(data) {
+        if (data.success) {
+            // Autenticación exitosa, mostrar el panel de envío de SMS y saldo de créditos
+            document.querySelector('.login-container').classList.add('hidden');
+            document.querySelector('.sms-container').classList.remove('hidden');
+            document.getElementById('credits').classList.remove('hidden');
 
-                // Mostrar el saldo de créditos
-                document.getElementById('credits-count').textContent = `${data.credits} créditos disponibles.`;
-            } else {
-                alert('Error de autenticación. Por favor, verifica tu usuario y contraseña.');
-            }
-        })
-        .catch(error => console.error('Error:', error));
+            // Mostrar el saldo de créditos
+            document.getElementById('credits-count').textContent = `${data.credits} créditos disponibles.`;
+        } else {
+            alert('Error de autenticación. Por favor, verifica tu usuario y contraseña.');
+        }
+    };
+
+    const script = document.createElement('script');
+    script.src = authURL;
+    document.head.appendChild(script);
 });
 
-document.getElementById('sms-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const phoneNumbers = document.getElementById('phoneNumbers').value;
-    const message = document.getElementById('message').value;
-
-    // URL para enviar SMS
-    const smsURL = `https://www.interconectados.net/api2/?phonenumber=${phoneNumbers}&text=${message}&user=${username}&password=${password}`;
-
-    fetch(smsURL)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                alert('Mensaje enviado con éxito.');
-            } else {
-                alert(`Error al enviar el mensaje: ${data.error}`);
-            }
-        })
-        .catch(error => console.error('Error:', error));
-});
+// La lógica para enviar SMS se mantiene igual, ya que es una solicitud POST y no se ve afectada por CORS.
