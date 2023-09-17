7ff980a0535087a9a1c8dccbdb53a233a66cdcf2
Angel Romero <mrprogramador21@gmail.com>
2023-09-17 17:39:07 -0400

    sms22

--- sms/script.js
+++ sms/script.js
@@ -26,4 +26,45 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     document.head.appendChild(script);
 });
 
-// La lógica para enviar SMS se mantiene igual, ya que es una solicitud POST y no se ve afectada por CORS.
+document.getElementById('sms-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const phoneNumbers = document.getElementById('phoneNumbers').value;
+    const message = document.getElementById('message').value;
+
+    // URL para enviar SMS
+    const smsURL = `https://www.interconectados.net/api2/?phonenumber=${phoneNumbers}&text=${message}&user=demo&password=demo`;
+
+    fetch(smsURL)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Mensaje enviado con éxito.');
+            } else {
+                alert(`Error al enviar el mensaje: ${data.error}`);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+document.getElementById('check-credits').addEventListener('click', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // URL para consultar créditos con JSONP
+    const creditsURL = `https://www.interconectados.net/api2/get.asp?user=${username}&password=${password}&get=credits&callback=handleCredits`;
+
+    // Agregar una función de callback para manejar la respuesta
+    window.handleCredits = function(data) {
+        if (data.success) {
+            // Mostrar los créditos disponibles
+            alert(`${data.credits} créditos disponibles.`);
+        } else {
+            alert(`Error al consultar los créditos: ${data.error}`);
+        }
+    };
+
+    const script = document.createElement('script');
+    script.src = creditsURL;
+    document.head.appendChild(script);
+});
